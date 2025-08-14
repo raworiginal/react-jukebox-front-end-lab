1e5241859dd426a289a99652b64e42e25b194e96
raworiginal <randall.a.wolfe@gmail.com>
2025-08-14 14:14:24 -0700

    wrote the trackService.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-// src/App.jsx
-
 const App = () => {
-  return <h1>Hello world!</h1>;
+	return <h1>Hello world!</h1>;
 };
 
 export default App;
-
---
+++ src/services/trackService.js
@@ -0,0 +1,53 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+const index = async () => {
+	try {
+		const res = await fetch(BASE_URL);
+		return res.json();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const create = async (formData) => {
+	try {
+		const res = await fetch(BASE_URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formData),
+		});
+		return res.json();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const update = async (formData, trackId) => {
+	try {
+		const res = await fetch(`${BASE_URL}/${trackId}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formData),
+		});
+		return res.json();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const deleteTrack = async (trackId) => {
+	try {
+		const res = await fetch(`${BASE_URL}/${trackId}`, {
+			method: "DELETE",
+		});
+		return res.json();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export { index, create, update, deleteTrack };
